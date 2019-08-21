11bc9a1de8def16d89a99a98b556ee52c57de8ef
bboyyu51 <bbo51@icloud.com>
2019-08-21 23:34:55 +0900

    Create Global channel

---
+++ src/bboyyu51/channeltalk/channel/GlobalChannel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace bboyyu51\channeltalk\channel;
+
+use pocketmine\Player;
+
+class GlobalChannel{
+
+    /** @var int */
+    protected $id = 0;
+
+    /** @var string */
+    protected $name = "Global";
+}
