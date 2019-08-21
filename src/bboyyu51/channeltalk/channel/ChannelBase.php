c7f87d1022b4caf21554f4a47b43bae2b015cccf
bboyyu <bbo51@icloud.com>
2019-08-21 17:52:46 +0900

    edit

--- src/bboyyu51/channeltalk/channel/ChannelBase.php
+++ src/bboyyu51/channeltalk/channel/ChannelBase.php
@@ -2,6 +2,25 @@
 
 namespace bboyyu51\channeltalk\channel;
 
+use pocketmine\Player;
+
 abstract class ChannelBase{
-    
+
+    /** @var int */
+    protected $id;
+
+    /** @var string */
+    protected $name;
+
+    protected $member;
+
+    /** 
+     * Send message to channel
+     * 
+     * @param Player $sender
+     * @param string $message
+     */
+    public function send(Player $sender, string $message): void{
+        
+    }
 }
