8c346d66aa504071b629cd1fcbbf9cc8c3a018d6
bboyyu51 <bbo51@icloud.com>
2019-08-21 22:48:29 +0900

    TalkManager instance

--- src/bboyyu51/channeltalk/Main.php
+++ src/bboyyu51/channeltalk/Main.php
@@ -5,5 +5,16 @@ namespace bboyyu51\channeltalk;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PlugunBase{
-
+    public function onEnable(){
+        new TalkManager();
+    }
+    
+    /**
+     * Get TalkManager instance
+     * 
+     * @return TalkManager
+     */
+    public function getManager(): TalkManager{
+        return TalkManager::getInatance();
+    }
 }
--- src/bboyyu51/channeltalk/TalkManager.php
+++ src/bboyyu51/channeltalk/TalkManager.php
@@ -4,4 +4,19 @@ namespace bboyyu51\channeltalk;
 
 class TalkManager{
     
+    /** @var self */
+    private static $instance;
+    
+    /** 
+     * Get TalkManager instance
+     *
+     * @return self
+     */
+    public static function getInstance(): self{
+        return self::$instance;
+    }
+    
+    public function __construct(){
+        self::$instance = $this;
+    }
 }
